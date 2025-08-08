348b68df7b7a0fad0d4c5121c22f2f733284bf24
Ashish Rana <ashishrana288@gmail.com>
2025-08-08 17:55:52 +0530

    added the dashboard code

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,11 +1,23 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
+use Illuminate\Container\Attributes\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 Route::resource('/users', UserController::class);
+
+
+    Route::post('login', [AuthController::class, 'login']);
+
+    Route::group(['middleware' => 'api'], function(){
+        Route::post('logout', [AuthController::class, 'logout']);
+        Route::post('refresh', [AuthController::class, 'refresh']);
+        Route::post('me', [AuthController::class, 'me']);
+});
